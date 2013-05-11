2d255109158d60c18da245d97d166744437b52d8
John Else <john.else@citrix.com>
2013-05-11 01:43:27 +0100

    Add helper function for buffered reads
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/mpd_client.ml
+++ lib/mpd_client.ml
@@ -13,6 +13,25 @@ module Make(Io: Mpd_transport.IO) = struct
           ~offset:(offset + chars_written)
           ~length:(length - chars_written))
 
+  let read_all ~connection =
+    let read_length = 1024 in
+    let read_data = String.create read_length in
+    let rec read_all' ~buffer =
+      Io.read connection.sock read_data 0 read_length
+      >>= (fun chars_read ->
+        if chars_read = 0
+        then return (Buffer.contents buffer)
+        else if chars_read < read_length then begin
+          Buffer.add_substring buffer read_data 0 chars_read;
+          return (Buffer.contents buffer)
+        end else begin
+          Buffer.add_string buffer read_data;
+          read_all' ~buffer
+        end)
+    in
+    let buffer = Buffer.create 0 in
+    read_all' ~buffer
+
   let send_raw ~connection ~data =
     let formatted_data = Printf.sprintf "%s\n" data in
     let length = String.length formatted_data in
