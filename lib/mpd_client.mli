9622c55c977114887f3a1bc6a59d9a70d7e235e6
John Else <john.else@citrix.com>
2013-05-20 01:00:09 +0100

    Add Connection.version_of to interface
    Signed-off-by: John Else <john.else@citrix.com>

--- lib/mpd_client.mli
+++ lib/mpd_client.mli
@@ -1,6 +1,7 @@
 module Make : functor (Io: Mpd_transport.IO) -> sig
   module Connection : sig
     type t
+    val version_of : connection:t -> string
   end
 
   val connect : addr:Io.sockaddr -> Connection.t Io.t
