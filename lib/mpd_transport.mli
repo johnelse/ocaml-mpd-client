36bc898012810f5ba088a5d8c907aceaee5ca565
John Else <john.else@citrix.com>
2013-07-05 03:09:10 +0100

    Add mpd_transport.mli
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ lib/mpd_transport.mli
@@ -0,0 +1,15 @@
+module type IO = sig
+  type 'a t
+  val (>>=) : 'a t -> ('a -> 'b t) -> 'b t
+  val (>|=) : 'a t -> ('a -> 'b) -> 'b t
+  val return : 'a -> 'a t
+
+  type sockaddr
+  type file_descr
+
+  val open_socket : sockaddr -> file_descr t
+  val close_socket : file_descr -> unit t
+
+  val read : file_descr -> string -> int -> int -> int t
+  val write : file_descr -> string -> int -> int -> int t
+end
