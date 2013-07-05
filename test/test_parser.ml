431257860285bd71300d1a4fb8855e9f4960da52
John Else <john.else@citrix.com>
2013-07-05 02:39:09 +0100

    Add some more parser tests
    Signed-off-by: John Else <john.else@citrix.com>

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -1,5 +1,25 @@
 open OUnit
 
+let test_parse_ok =
+  let test ~msg ~input ~expected_output () =
+    let parsed = Mpd_parser.parse_ok input in
+    assert_equal ~msg parsed expected_output
+  in
+  "test_parse_ok" >:::
+    [
+      "test_empty_response" >::
+        test ~msg:"Parsing an empty OK response"
+          ~input:"OK\n"
+          ~expected_output:(Mpd_parser.Ok []);
+      "test_response_with_data" >::
+        test ~msg:"Parsing an OK response with some data"
+          ~input:"key1: value1\nkey2: value2\nOK\n"
+          ~expected_output:(Mpd_parser.Ok [
+            "key1", "value1";
+            "key2", "value2";
+          ]);
+    ]
+
 let test_parse_ack =
   let test ~msg ~input ~expected_output () =
     let parsed = Mpd_parser.parse_ack input in
@@ -21,6 +41,7 @@ let test_parse_ack =
 let base_suite =
   "base_suite" >:::
     [
+      test_parse_ok;
       test_parse_ack;
     ]
 
